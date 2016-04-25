2498949ed61172d4b8537aa7ac95bbf3e14ee2ad
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-04-25 17:53:32 +0200

    add the ability to draw speakers from the dataset

--- website/gui.js
+++ website/gui.js
@@ -16,7 +16,11 @@ include('./devices.js');
 GUI = (function (global) {
     var speaker_list = $("#speaker_list");
     var draw_area = $("#draw_area");
-
+    /* Scale factor for In Real Life to Virtual
+     * Default: 1:1
+     */
+    var scale_irl=1;
+    var scale_virt=1;
     var init = function () {
         draw_speakers_from_data();
     };
@@ -36,13 +40,17 @@ GUI = (function (global) {
         });
     };
     var clear = function () {
-        clear_speaker_list();
-        clear_draw_area();
+        empty_speaker_list();
+        empty_draw_area();
     };
     /* Draw speakers from CLIENT data */
     var draw_speakers_from_data = function () {
-        //draw_speakers();
-        //
+        empty_draw_area(); // Clear the draw area
+        var nr_of_objects=0;
+        var object_tmp = $("<div class='object' id='object_tmp'></div>").hide().appendTo("body"); //add temp object
+        var object_width = parseInt($('.object').css('width').replace('px',''));
+        var object_height= parseInt($('.object').css('height').replace('px',''));
+        object_tmp.remove(); //remove temp object
         //get all draws speakers
         //Delete unused speakers
         $('.speaker').each(function(key, value){
@@ -54,9 +62,49 @@ GUI = (function (global) {
         //Loop all speakers
         $.each(CLIENT.get_online(), function(key, value){
             //has object(s)?
+            var speaker_name = key;
             if (!isEmpty(value)) { // Has object(s)
-                console.log("Has object?");
+                console.log("Has object");
                 //do things
+                //loop objects
+                nr_of_objects=0;
+                $.each(CLIENT.get_online()[key], function(obj, obj_value){
+                    if (!nr_of_objects) {
+                        //first object
+                        console.log("First object");
+                        //already drawn?
+                        if (isEmpty($('#'+obj))) {
+                            //No, speaker not yet drawn
+                            //- draw object in center of draw area
+                            //- draw speaker from that x and y axis
+                            console.log('obj '+obj+' not drawn');
+                            //var width=speaker_list.width();
+                            //var height=speaker_list.height();
+                            draw_object(obj, ((draw_area.width()-object_width)/2), ((draw_area.height()-object_height)/2));
+                            //var speaker_width=parseInt($('.speaker').css('width').replace('px',''));
+                        }
+                        //draw speaker from object
+                        del_speaker(speaker_name); //delete speaker
+                        var left =$('#'+obj).offset().left-draw_area.offset().left-rectangular(obj_value.distance, obj_value.angle).x;
+                        var top  =$('#'+obj).offset().top-draw_area.offset().top+rectangular(obj_value.distance, obj_value.angle).y;
+                        draw_speaker(speaker_name, draw_area, left, top); //draw speaker
+                    } else {
+                        //not first obj
+                        //object already drawn?
+                        //yes, do nothing (speaker is alrady drwn in field)
+                        //no: draw the object seen from the speaker
+                        if (isEmpty($('#'+obj))) {
+                            //draw the object from speaker
+                            //del_object(obj);//delete object
+                            var left =$('#'+speaker_name).offset().left-draw_area.offset().left+rectangular(obj_value.distance, obj_value.angle).x;
+                            var top  =$('#'+speaker_name).offset().top-draw_area.offset().top-rectangular(obj_value.distance, obj_value.angle).y;
+                            draw_object(obj, left, top); //draw object
+                        }
+                    }
+                    console.log(obj);
+                    console.log(obj_value);
+                    nr_of_objects++;
+                }, speaker_name);
             } else { // Has no objects
                 console.log("Has no object");
                 if (isEmpty($('#'+key))) {//not drawn
@@ -105,20 +153,37 @@ GUI = (function (global) {
         });
     };
     /* Delete single speaker */
-    var del_speaker = function (speaker) {
-        $('#'+speaker).remove();
+    var del_speaker = function (speaker_name) {
+        $('#'+speaker_name).remove();
     };
-    var clear_speaker_list = function () {
-        speaker_list.empty();
+    var draw_object = function (object_name, off_left=0, off_top=0) {
+        console.log('draw at: '+off_left+' '+off_top);
+        var object_class = "<div class='object' id='"+object_name+"'>"+object_name+"</div>";
+        draw_area.append( object_class );
+        var object = $('#'+object_name);
+        //set offset
+        object.offset({ top: draw_area.offset().top+off_top, left: draw_area.offset().left+off_left});
+        //make dragable
+        object.draggable({
+            containment: "#draw_area",
+            scroll: false,
+            snap: "#draw_area",
+            snapMode: "both"
+        });
     };
-
-    var add_draw_area = function (obj) {
+    /* Delete single object */
+    var del_object = function (object_name) {
+        $('#'+object_name).remove();
     };
-    var del_draw_area = function (obj) {
+    /* Clear objects and speakers respectively */
+    var empty_speaker_list = function () {
+        $('#speaker_list .speaker').remove();
+        $('#speaker_list .object').remove();
     };
-    var clear_draw_area = function () {
+    var empty_draw_area = function () {
+        $('#draw_area .speaker').remove();
+        $('#draw_area .object').remove();
     };
-
     var polar = function(x, y) {
         var polar = {
             distance:0,
@@ -182,6 +247,14 @@ GUI = (function (global) {
         }
         return pos;
     };
+    /* Convert virt value to IRL value with scale */
+    var scale_to_irl = function (value) {
+        return ((value*scale_irl)/scale_virt);
+    };
+    /* Convert IRL value to VIRT value with scale */
+    var scale_to_virt = function (value) {
+        return ((value*scale_virt)/scale_irl);
+    };
     // Converts from degrees to radians.
     Math.radians = function(degrees) {
         return degrees * Math.PI / 180;
@@ -195,11 +268,14 @@ GUI = (function (global) {
     return {
         init: init,
         draw_speakers: draw_speakers,
+        draw_object:draw_object,
         draw_speakers_from_data: draw_speakers_from_data,
         clear: clear,
         polar: polar,
         calc_next_speaker_list_pos:calc_next_speaker_list_pos,
-        rectangular: rectangular
+        rectangular: rectangular,
+        scale_to_irl:scale_to_irl,
+        scale_to_virt:scale_to_virt
     };
 })(window);
 
