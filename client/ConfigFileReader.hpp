d1c0d1105b11940509304c8f465807551ee0ba18
gerwie <brianvanzwam@gmail.com>
2016-05-06 20:51:56 +0200

    added Config File parser. it can read a config file with data for the client. + there is a test to verified the working of the Config File Parser

---
+++ client/ConfigFileReader.hpp
@@ -0,0 +1,66 @@
+#ifndef CONFIG_FILE_READER_HPP
+#define CONFIG_FILE_READER_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "libs/jzon/Jzon.h"
+
+const int NUMBEROFBROKERS {4};
+const std::string CONFIGPATH_IN {"config/config.js"};
+const std::string CONFIGPATH_OUT {"config/config.json"};
+
+
+
+struct Broker
+{
+	std::string name = "Mosquitto" ;
+	std::string uri  = "test.mosquitto.org";
+	int port = 1883 ;
+	int port_encrypted = 8883;
+	int port_encrypted_certificate = 8884;
+	int port_ws = 8080;
+	int port_ws_encrypted = 8081;
+};
+
+
+class ConfigFileReader
+{
+public:
+	ConfigFileReader();
+	~ConfigFileReader();
+
+	void getDataFromFile();
+	void writeDataToFile();
+	void parseData();
+	std::string getProjectName(bool fullname = true);
+	std::string getVersion();
+	std::string getSpeakerName();
+	std::string getWebsiteName();
+	Broker getBroker(int broker = 1);
+	
+	void printJsonData();	
+	void printBroker(int broker);
+
+
+protected:
+
+private:
+	std::string FileData;
+	std::string JsonData;
+	std::string ProjectName;
+	std::string ShortProjectName;
+	int VersionMajor, VersionMinor, VersionRevision;
+	std::string SpeakerName;
+	std::string WebsiteName;
+	int BrokerToUse; 
+
+	Jzon::Parser _filereader;
+	Broker brokers[NUMBEROFBROKERS];
+};
+
+
+
+#endif
